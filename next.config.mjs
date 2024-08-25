c9e8c0ce693bcc98f42f22f93fe0ffff959820ac
Aman Gautam <amangautam2128@gmail.com>
2024-08-26 00:35:35 +0530

    Added route

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/sign-in",
+        permanent: true,
+      },
+    ];
+  },
   typescript:{
     ignoreBuildErrors:true
   },
